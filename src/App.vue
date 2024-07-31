ba8e0844a5d31c92bae9b5c903e65abfdc537db7
Mike Bellika <mb@moxso.com>
2024-07-31 09:55:02 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ReproComponent from "./components/ReproComponent.vue";
+</script>
+
+<template>
+  <h1>This is fine</h1>
+  <ReproComponent :text="'hi'" :subheading="{ text: 'hi' }" />
+
+  <h1>This should give an error</h1>
+  <ReproComponent :text="'hi'" />
+
+  <h1>This is also fine</h1>
+  <ReproComponent />
+</template>
---
+++ src/components/ReproComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<
+  | { text: string; subheading: { text: string } }
+  | { text?: undefined; subheading?: undefined }
+>();
+const testInScript = computed(() => {
+  if (props.text) {
+    // This works
+    return props.subheading.text;
+  }
+  return "default value";
+});
+</script>
+<template>
+  <div v-if="text">
+    <h1>{{ text }}</h1>
+    <!-- This doesn't work -->
+    {{ subheading.text }}
+    <!-- '__VLS_ctx.subheading' is possibly 'undefined'. -->
+  </div>
+  <div v-else>{{ testInScript }}</div>
+</template>
